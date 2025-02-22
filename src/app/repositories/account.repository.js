125fb1b4d5a5e4ff498a6a474fa3273c6595b56f
Mateo Loaiza Rios <mateo@nereus.io>
2025-02-22 11:35:11 -0500

    initial commit

---
+++ src/app/repositories/account.repository.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    user: process.env.DB_POSTGRES_USER,
+    password: process.env.DB_POSTGRES_PASSWORD,
+    database: process.env.DB_POSTGRES_DATABASE_ACCOUNT,
+    host: process.env.DB_POSTGRES_HOST,
+    port: process.env.DB_POSTGRES_ACCOUNT_PORT
+})
+
+const accountRepository = {
+    getAccounts: async () => {
+        var results = await pool.query('SELECT * FROM account')
+        return results.rows
+    },
+    getAccountById: async (id) => {
+        var results = await pool.query('SELECT * FROM account WHERE id = $1', [id])
+        return results.rows
+    },
+    addAccount: async (amount, customerId, fullname) => {
+        var results = await pool.query('INSERT INTO account (amount, customerid,fullname) VALUES ($1, $2, $3) RETURNING *', [amount, customerId, fullname])
+        return results.rows
+    }
+}
+
+module.exports = accountRepository;
